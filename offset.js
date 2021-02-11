976328a21341e3fd80717fa38c7f671dae4f875e
MunchDuster <72060614+MunchDuster@users.noreply.github.com>
2021-02-12 08:34:01 +1300

    Add files via upload

---
+++ offset.js
@@ -0,0 +1,16 @@
+codecs[codecs.length] = {
+  decode: (msg,move) => {
+    var newstr = "";
+    for (var i = 0; i < msg.length; i++) {
+      newstr += String.fromCharCode(msg.charCodeAt(i) - move);
+    }
+    return newstr;
+  },
+  encode: (msg,move) => {
+    var newstr = "";
+    for (var i = 0; i < msg.length; i++) {
+      newstr += String.fromCharCode(msg.charCodeAt(i) + move);
+    }
+    return newstr;
+  },
+};
