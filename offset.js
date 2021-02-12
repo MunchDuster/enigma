6276b2b02fe04fe8ca415dd04b4fd46ef834e53f
MunchDuster <72060614+MunchDuster@users.noreply.github.com>
2021-02-12 15:20:02 +1300

    bug fixes, add rotate coder

--- offset.js
+++ offset.js
@@ -1,6 +1,7 @@
 codecs[codecs.length] = {
   decode: (msg,move) => {
     var newstr = "";
+	console.log("unoffsetting by " + move);
     for (var i = 0; i < msg.length; i++) {
       newstr += String.fromCharCode(msg.charCodeAt(i) - move);
     }
@@ -8,6 +9,7 @@ codecs[codecs.length] = {
   },
   encode: (msg,move) => {
     var newstr = "";
+	console.log("offsetting by " + move);
     for (var i = 0; i < msg.length; i++) {
       newstr += String.fromCharCode(msg.charCodeAt(i) + move);
     }
---
+++ rotate.js
@@ -0,0 +1,16 @@
+codecs[codecs.length] = {
+  decode: (msg, move) => {
+    var newstr = "";
+    for (var i = 0; i < msg.length; i++) {
+      newstr += msg[i - move % msg.length];
+    }
+    return newstr;
+  },
+  encode: (msg, move) => {
+    var newstr = "";
+    for (var i = 0; i < msg.length; i++) {
+      newstr += msg[i + (move % msg.length)];
+    }
+    return newstr;
+  },
+};
